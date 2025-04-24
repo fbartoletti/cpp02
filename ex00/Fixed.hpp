71ade15070947c7605542a7d330f0a7c8161b7b4
fbartole <thekingpipp8@gmail.com>
2025-04-24 15:55:40 +0200

    ex00 push

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	int value;
+	static const int fractionalBits;
+public:
+	Fixed();
+	Fixed(const Fixed& rhs);
+	Fixed& operator=(const Fixed& rhs);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
